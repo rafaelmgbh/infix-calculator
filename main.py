f7ca09bfa87bfbbcc9f484ab06190d6af2a1bccb
rafaelmgbh <rafaelsantosdearaujo@gmail.com>
2022-07-28 07:48:55 -0300

    add log function on file

--- main.py
+++ main.py
@@ -18,11 +18,14 @@ $ infixcalc.py mul 10 5
 50
 
 
+os resultados serão salvos em 'infixcalc.log' .
 """
 
 __version__ = "0.1.0"
 
+import os
 import sys
+from datetime import datetime
 arguments = sys.argv[1:]
 
 if not arguments:
@@ -63,5 +66,11 @@ elif operation == 'mul':
     result = n1*n2
 elif operation == 'div':
     result = n1/n2
+
+path = os.curdir
+filepath = os.path.join(path, 'infixcalc.log')
+timestamp = datetime.now().isoformat()
+with open(filepath, 'a') as file_:
+    file_.write(f"{timestamp} - {operation},{n1},{n2} = {result}\n")
 
 print(f'O Resultado é {result}')
